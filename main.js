14736ced7d8a468c9e913a566b40bb68be695af2
medjugomes <136620326+medjugomes@users.noreply.github.com>
2024-05-29 09:36:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+var caixaPrincipal = document.querySelector("cx-principal");
+var cxPerguntas = document. querySelector("cx-perguntas");
+var cxAlternativas = document. querySelector ("cx-alternativas");
+var cxResultado = document. querySelector ("cx-resultado");
+var txResultado = document. querySelector("tx-resultado");
+var perguntas = [
+    {
+        enunciado = "A IA pode automatizar tarefas repetitivas.",
+        alternativa = [
+         "Vai eliminar tarefas monotonas",
+         "Vai tirar muitos empregos"
+        ]
+    },
+
+
+{
+   enunciado = "A IA pode tomar decisões baseadas em dados.",
+   alternativa = [
+   "Maravilhoso",
+   "Assustador"
+   ]
+
+},
+{
+  enunciado = "A IA pode substituir certos empregos.",
+  alternativa = [
+  "Não vejo problemas",
+  "É o fim dos empregos como conhecemos hoje"
+  ]
+
+},
+{
+  enunciado = "A IA pode ajudar a prever tendências de mercado.",
+  alternativa = [
+ "Incrível",
+ "Sinistro "
+  ]
+
+},
+{
+    enunciado = "A IA pode trabalhar 24/7 sem pausas.",
+    alternativa = [
+   "Surpreendente",
+   "Mão de obra barata"
+    ]
+}
+]
